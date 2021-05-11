63edb86537b62dc7ad246bb357540c6f0f1d539a
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-05-11 15:36:10 -0300

    criado model users

---
+++ src/database/migrations/20210511175012-create-users.js
@@ -0,0 +1,67 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password_hash: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      rua: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      numero: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      cidade: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      bairro: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      uf: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      foto_url: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      is_admin: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users')
+  }
+}
---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcryptjs'
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        rua: Sequelize.STRING,
+        numero: Sequelize.STRING,
+        bairro: Sequelize.STRING,
+        cidade: Sequelize.STRING,
+        uf: Sequelize.STRING,
+        foto_url: Sequelize.STRING
+      },
+      {
+        sequelize
+      }
+    )
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8)
+      }
+    })
+
+    return this
+  }
+
+  async chekPassword(password) {
+    return await bcrypt.compare(password, this.password_hash)
+  }
+}
+
+export default User
