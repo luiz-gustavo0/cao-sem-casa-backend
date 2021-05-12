5e7736ce6ec518ec532573545721d5bb9eddca9b
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-05-11 23:51:20 -0300

    adiciona endpoints de AnimalController

--- src/controllers/AnimalController.js
+++ src/controllers/AnimalController.js
@@ -7,6 +7,9 @@ class AnimalController {
     const { page = 1 } = request.query
 
     const animals = await Animal.findAll({
+      where: {
+        status: 'nao adotado'
+      },
       limit: 20,
       offset: (page - 1) * 20
     })
@@ -14,10 +17,22 @@ class AnimalController {
     return response.json(animals)
   }
 
+  async show(request, response) {
+    const userId = request.params.id
+    try {
+      const animal = await Animal.findByPk(userId)
+      if (!animal) {
+        return response.status(400).json({ error: 'Animal não encontrado.' })
+      }
+      return response.json(animal)
+    } catch (err) {
+      return response.status(400).json(err)
+    }
+  }
+
   async create(request, response) {
     const file = request.file
 
-    console.log(file)
     try {
       if (!file) {
         return response
@@ -50,6 +65,62 @@ class AnimalController {
       })
 
       return response.status(201).json(animal)
+    } catch (err) {
+      return response.status(400).json(err)
+    }
+  }
+
+  async update(request, response) {
+    // const file = request.file
+    const animalId = request.params.id
+
+    const schema = yup
+      .object()
+      .shape({
+        name: yup.string(),
+        peso: yup.number().positive(),
+        idade: yup.number().positive(),
+        raca: yup.string(),
+        tipo: yup.string(),
+        vacinado: yup.string(),
+        vermifugado: yup.string(),
+        castrado: yup.string()
+      })
+      .noUnknown()
+    try {
+      const animal = await Animal.findByPk(animalId)
+
+      if (!animal) {
+        return response.status(400).json({ error: 'Animal não encontrado.' })
+      }
+
+      const validFields = await schema.validate(request.body, {
+        abortEarly: false,
+        stripUnknown: true
+      })
+
+      const newData = await animal.update({ ...validFields })
+
+      return response.status(200).json(newData)
+    } catch (err) {
+      console.log('ERROR', err)
+      return response.status(400).json(err)
+    }
+  }
+
+  async delete(request, response) {
+    const animalId = request.params.id
+
+    try {
+      const animal = await Animal.findByPk(animalId)
+
+      if (!animal) {
+        return response.status(400).json({ error: 'Animal não encontrado.' })
+      }
+
+      await animal.destroy()
+
+      return response.json({ message: 'Deletado com sucesso' })
     } catch (err) {
       return response.status(400).json(err)
     }
--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,9 @@ routes.get('/users/:id', UserController.show)
 routes.post('/users', UserController.create)
 
 routes.get('/pets', AnimalController.index)
+routes.get('/pets/:id', AnimalController.show)
 routes.post('/pets', upload.single('file'), AnimalController.create)
+routes.put('/pets/:id', AnimalController.update)
+routes.delete('/pets/:id', AnimalController.delete)
 
 export default routes
