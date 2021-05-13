78dce0b36ba262d3bffe72f1625b6e341dbf9561
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-05-12 23:23:00 -0300

    adicionacors e helmet

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
 
 import routes from './routes'
 
@@ -10,6 +12,8 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(helmet())
+app.use(cors())
 app.use(morgan('dev'))
 
 app.use(routes)
